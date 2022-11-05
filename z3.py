13984837ab9aa7f9ecc9137e0c46663d8887baec
georrgiok <111180954+georrgiok@users.noreply.github.com>
2022-11-05 23:25:34 +0300

    Update z3.py

--- z3.py
+++ z3.py
@@ -1,26 +1,29 @@
 # Создайте скрипт бота, который находит ответы на фразы по ключу в словаре. Бот должен, как минимум, отвечать на фразы «привет», «как тебя зовут». Если фраза ему неизвестна, он выводит соответствующую фразу.
-«как тебя зовут?» –> меня зовут Анатолий
+#«как тебя зовут?» –> меня зовут Анатолий
 import json
-print('Simple bot for GB homework')
-FILE_NAME = '2_z3_answers.json'
+print('Примитивный чат-бот')
+FILE_NAME = 'answers.json'
 try: 
-    f = open(FILE_NAME, 'r')
+    f = open(FILE_NAME, 'r', encoding="utf8")
 except IOError:
-    print('file '+FILE_NAME+' does not exist!')
+    print('Файл с ответами для чат бота ('+FILE_NAME+') Не существует!')
 else:
     def answer(req, dict):
         from random import randint
+        print(req)
         if req in dict['inputs']:
+
             if dict['inputs'][req] in dict['answers']:
                 tmp = dict['answers'][dict['inputs'][req]]
                 return tmp[randint(0,len(tmp)-1)]
-        return dict["err"][0, len(dict["err"])]
+        return dict["err"][randint(0, len(dict["err"])-1)]
 
     bot_answers = json.loads(f.read())
+    f.close()
     req = ''
-    print('Type "bye" for exit bot')
+    print('Напишите боту "Пока" чтобы выйти. ')
     while 1:
-        req = input('write something to the bot: ').lower()
+        req = input('Напишите что-нибудь боту: ').lower()
         print(answer(req, bot_answers))
-        if req == "bye":
+        if req == "пока":
             break
